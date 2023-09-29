b0399e64095c09ab31291357f72d750f76dcdb94
Ankit2004tailor <146409875+Ankit2004tailor@users.noreply.github.com>
2023-09-29 11:41:46 +0530

    day 2

---
+++ hello.py
@@ -0,0 +1,13 @@
+print(2+3)
+print("hello")
+print("ayush")
+#  ayush
+a=1
+s=("ankit")
+z=2.445
+print(a,s,z)
+# priting data type of variables
+print(type(z))
+sum1=a+z
+mul1=a*z
+print(sum1,mul1)
